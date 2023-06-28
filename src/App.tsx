8fc710a994161eda40caf9f26e3ebf9c82f5ff13
Darcyxwz <1003669366@qq.com>
2023-06-28 09:41:38 +0800

    export default FINISH

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  const [count0, setCount0] = useState(0);
-  const [count1, setCount1] = useState(0);
-  function handleClick0() {
-    setCount0(count0 + 1);
-  }
-  function handleClick1() {
-    setCount1(count1 + 1);
-  }
+  
   return (
-    <>
-      <h1>xwz's world</h1>
-      <div>
-        <button onClick = {handleClick0}>click me {count0} times!</button>
+    <div className = "background">
+      <h1>React Todo</h1>
+      <div className = "thingsToDo">
+        <ThingsToDo />
       </div>
-      <div>cl{count0 + count1}</div>
-      <div>  
-        <button onClick = {handleClick1}>click me {count1} times!</button>
+      <div className = "tasksToAdd">
+        <TasksToAdd />
       </div>
-    </>
+    </div>
   )
+}
+
+function ThingsToDo() {
+  return (
+    <button>1</button>
+  );
+}
+
+function TasksToAdd() {
+  return (
+    <button>2</button>
+  );
 }
